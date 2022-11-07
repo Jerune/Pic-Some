e7b2adfddbe1c8dbab270a6cbde9c642434288dd
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-11-07 12:05:57 +0100

    feat: Fill cart icon in header when there are items in the cart
    the cart icon in the header does now use context to validate if it includes any items and changes the icon accordingly

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+// @ts-nocheck
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { Context } from '../../Context'
 
 function Header () {
+  const { cartItems } = useContext(Context)
+
   return (
         <header>
             <NavLink to='/'><h2>Pic Some</h2></NavLink>
-            <NavLink to='cart'><i className="ri-shopping-cart-line ri-fw ri-2x"></i></NavLink>
+            <NavLink to='cart'>{cartItems.length > 0 ? <i className="ri-shopping-cart-fill ri-fw ri-2x"></i> : <i className="ri-shopping-cart-line ri-fw ri-2x"></i>}</NavLink>
         </header>
   )
 }
