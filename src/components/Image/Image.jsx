361d5e57011fe74d9d435363c21f1b31a97867a7
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-11-07 10:49:41 +0100

    feat: Images are now showing favorite state
    When image is favorited it shows a permanent filled heart at the top left of the image

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -3,13 +3,21 @@ import PropTypes from 'prop-types'
 
 function Image ({ className, img, toggle }) {
   const [hovered, setHovered] = useState(false)
-  const heartIcon = hovered && <i className="ri-heart-line favorite" onClick={() => toggle(img.id)}></i>
+
+  function heartIcon () {
+    if (img.isFavorite) {
+      return <i className="ri-heart-fill favorite" onClick={() => toggle(img.id)}></i>
+    } else if (!img.isFavorite && hovered) {
+      return <i className="ri-heart-line favorite" onClick={() => toggle(img.id)}></i>
+    }
+  }
+
   const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
 
   return (
         <div className={`${className} image-container`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
             <img src={img.url} className="image-grid"/>
-            {heartIcon}
+            {heartIcon()}
             {cartIcon}
         </div>
   )
