dd1ee5f9ae59416abef0a62b3f956cbaa0b6b2f1
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-11-07 15:28:07 +0100

    refactor: Moved to variable instead of function for hovered state
    Now only the classname of icons depends on state instead of complete icon and it is placed in a simple variable instead of function

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -7,17 +7,11 @@ function CartItem ({ item }) {
   const [hovered, setHovered] = useState(false)
   const { removeItemFromCart } = useContext(Context)
 
-  function trashIcon () {
-    if (!hovered) {
-      return <i className="ri-delete-bin-line" onClick={() => removeItemFromCart(item)} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}></i>
-    } else if (hovered) {
-      return <i className="ri-delete-bin-fill" onClick={() => removeItemFromCart(item)} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}></i>
-    }
-  }
+  const iconClassName = hovered ? 'ri-delete-bin-fill' : 'ri-delete-bin-line'
 
   return (
         <div className="cart-item">
-            {trashIcon()}
+            <i className={iconClassName} onClick={() => removeItemFromCart(item)} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}></i>
             <img src={item.url} width="130px" />
             <p>$5.99</p>
         </div>
